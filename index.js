efceb6644e6275c5f8cf2d5093f3b05dc4feae2a
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-04-08 11:50:33 -0400

    fallback to 3000

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ app.get("/", async (req, res) => {
     }
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT ?? "3000", () => {
     console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
